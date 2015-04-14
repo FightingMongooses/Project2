8735651c631e6d31062320227e42c58dee0c436b
Zaephor <github@draconrose.com>
2015-04-14 16:59:39 -0700

    Corrected most JSHint complaints ahead of time, renamed 2 public directories to shorten things up a little

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
-/* jshint node: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, strict: true, undef: true, unused: true */
-var express = require('express');
+"use strict";
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
