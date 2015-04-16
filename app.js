674c40945296ce1659dbba9626b0637ef29644c2
Zaephor <github@draconrose.com>
2015-04-15 21:25:27 -0700

    Added mongoose, committing to test something

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ var favicon = require("serve-favicon");
 var logger = require("morgan");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
 var routes = require("./routes/index");
 var users = require("./routes/users");
 
 var app = express();
+
+// Mongodb connection
+mongoose.connect("mongodb://localhost/cpsc473fightingmongooses");
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
