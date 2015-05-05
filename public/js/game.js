df5267815b8c586cdd5acb5e2c22c8114697cd5c
Zaephor <github@draconrose.com>
2015-05-04 17:18:56 -0700

    Initial steps to integrate game mechanics and screens to whole system

--- public/js/game.js
+++ public/js/game.js
@@ -1,6 +1,26 @@
 "use strict";
 var game = {
     eventHandlers: function () {
+        // Override clicks
+        $("#send-place").click(function(event) {
+            event.preventDefault();
+            var x = $("#input-x").val();
+            var y = $("#input-y").val();
+            var card = $("#element").val();
+            // check x,y authenticity
+
+            var m = x + "" + y;
+//        socket.emit("chat message", m);
+            socket.emit("game:placeCard", {card:card, position:m});
+//            socket.emit("change turn");
+        });
+        // Socket event capturing
+        socket.on("game:updateBoard", function(data, pos){ // Place card on x/y
+            var posi = "label" + pos;
+            var img = "img" + pos;
+            document.getElementById(img).src = "data:image/png;base64,"+data;
+        });
+
     }
 };
 $(document).ready(function () {
--- sockets/game.js
+++ sockets/game.js
@@ -2,8 +2,61 @@
 // REF: http://liangzan.net/blog/blog/2012/06/04/how-to-use-exports-in-nodejs/
 
 module.exports = function (io,socket) {
+    var lobby = "lobby";
+    // Utility functions
+    function findClientsSocket(roomId, namespace) {
+        var res = [], ns = io.of(namespace ||"/");    // the default namespace is "/"
+
+        if (ns) {
+            for (var id in ns.connected) {
+                if(roomId) {
+                    var index = ns.connected[id].rooms.indexOf(roomId) ;
+                    if(index !== -1) {
+                        res.push(ns.connected[id]);
+                    }
+                } else {
+                    res.push(ns.connected[id]);
+                }
+            }
+        }
+        return res;
+    }
+    
     // Game functions
     socket.on("game:",function(msg){
         console.log("Game: "+msg);
+    });
+    socket.on("game:connect",function(msg){
+        var decode = validator.isValidToken(msg.token);
+        if(decode){
+            // User connected
+            socket.emit("chat message", "You are connected");
+            io.to(lobby).emit("chat message", usr + " connected");
+            // Join game lobby
+            socket.join(lobby);
+            // When another user joins, leave lobby, both join new room
+            var clients = findClientsSocket(lobby);
+            if(clients.length === 1){
+
+                // create new room object
+                var room = new Room("game" + numRooms, clients[0].username, socket.username, [0,0,0,0,0,0,0,0,0]);
+                rooms.push(room);
+
+                // remove first client from lobby and add to new room
+                clients[0].leave(lobby);
+                clients[0].join(room.name);
+
+                // remove second client from lobby and add to new room
+                socket.leave(lobby);
+                socket.join(room.name);
+
+                // increase room counter and start turn swapping
+                numRooms = numRooms +1;
+                clients[0].emit("change turn");
+            }
+        }
+    });
+    socket.on("game:placeCard",function(msg){ //Handle placing the card on the board
+
     });
 };
